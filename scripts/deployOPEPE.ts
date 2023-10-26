2b79a044b7e73fc52374982dc3c8aad31ef5b125
adamant-al <adamantim@yandex.ru>
2023-10-26 22:24:44 +0400

    chore: update comments and log messages

--- scripts/deployOPEPE.ts
+++ scripts/deployOPEPE.ts
@@ -14,48 +14,50 @@ async function main() {
 
   console.log(
     `Your account is ${signerAddress}, it's balance is ${ethers.formatEther(await ethers.provider.getBalance(signerAddress))} ETH.` +
-    " This account will create oPepe token and the oPepe price oracle." +
-    "\nDo you want to proceed?"
+    ' This account will create oPepe token and the oPepe price oracle.'
   );
 
-  const proceed = readline.question('Y/n\n');
+  const proceed = readline.question('Do you want to proceed? (Y/n)\n');
 
   if (!['y', 'yes'].includes(proceed.toLowerCase())) {
-    console.log('Bye')
+    console.log('Bye!\n');
     return;
+  } else {
+    console.log('Creating oPepe token..');
   }
 
   const oPepe = await ethers.deployContract("OErc20Delegator", [
-    PEPE_ADDRESS, // underlying token address (PEPE)
-    COMPTROLLER_ADDRESS, // Comptroller address
-    TOKEN_JUMP_RATE_MODEL_ADDRESS, // Interest rate model address
-    DEFAULT_OTOKEN_INITIAL_EXCHANGE_RATE_MANTISSA, // initial exchange rate mantissa
+    PEPE_ADDRESS, // Underlying token address (PEPE)
+    COMPTROLLER_ADDRESS,
+    TOKEN_JUMP_RATE_MODEL_ADDRESS, // Interest rate model
+    DEFAULT_OTOKEN_INITIAL_EXCHANGE_RATE_MANTISSA,
     "Onyx PEPE",
     "oPEPE",
-    DEFAULT_OTOKEN_DECIMALS, // oToken decimals
-    GOVERNANCE_TIMELOCK_ADDRESS, // admin address (Timelock)
-    OTOKEN_IMPLEMENTATION_ADDRESS, // implementation address (OErc20Delegate)
-    "0x" // become implementation signature (empty)
+    DEFAULT_OTOKEN_DECIMALS,
+    GOVERNANCE_TIMELOCK_ADDRESS, // Admin
+    OTOKEN_IMPLEMENTATION_ADDRESS, // OErc20Delegate
+    "0x" // Become implementation signature (empty)
   ]);
 
   await oPepe.waitForDeployment();
 
+  console.log('Creating oPepe price Oracle..');
   const oPepeOracleFactory = await ethers.getContractFactory('UniV3TwapOracle') as UniV3TwapOracle__factory;
 
   const oPepeOracle = await oPepeOracleFactory.deploy(
-    PEPE_ADDRESS, // PEPE token
-    PEPE_ETH_UNIV3_POOL, // PEPE/ETH Uni V3 Pool
+    PEPE_ADDRESS,
+    PEPE_ETH_UNIV3_POOL,
     3600,
   );
 
   await oPepeOracle.waitForDeployment();
 
   await oPepeOracle.setAdmin(
-    GOVERNANCE_TIMELOCK_ADDRESS as any // Timelock address
+    GOVERNANCE_TIMELOCK_ADDRESS as any
   );
 
-  console.log('Deployed oToken oPEPE, address: ', await oPepe.getAddress());
-  console.log('Deployed oracle for oPEPE, address: ', await oPepeOracle.getAddress());
+  console.log('Deployed oToken oPEPE, address:', await oPepe.getAddress());
+  console.log('Deployed oracle for oPEPE, address:', await oPepeOracle.getAddress());
 }
 
 main().catch((error) => {
