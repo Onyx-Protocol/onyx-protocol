86ed4235cc113731ad237827b5431bba75551075
Alex <sampekmoy@gmail.com>
2023-10-26 17:54:07 +0400

    feat: consts

--- scripts/deployOPEPE.ts
+++ scripts/deployOPEPE.ts
@@ -1,17 +1,40 @@
 import { ethers } from "hardhat";
 import { UniV3TwapOracle, UniV3TwapOracle__factory } from "../typechain-types";
+import {
+  COMPTROLLER_ADDRESS, DEFAULT_OTOKEN_DECIMALS,
+  DEFAULT_OTOKEN_INITIAL_EXCHANGE_RATE_MANTISSA, GOVERNANCE_TIMELOCK_ADDRESS, OTOKEN_IMPLEMENTATION_ADDRESS,
+  PEPE_ADDRESS, PEPE_ETH_UNIV3_POOL,
+  TOKEN_JUMP_RATE_MODEL_ADDRESS
+} from "./constants";
+import readline from 'readline-sync'
 
 async function main() {
+  const [account] = await ethers.getSigners();
+  const signerAddress = await account.getAddress();
+
+  console.log(
+    `Your account is ${signerAddress}, it's balance is ${ethers.formatEther(await ethers.provider.getBalance(signerAddress))} ETH.` +
+    " This account will create oPepe token and the oPepe price oracle." +
+    "\nDo you want to proceed?"
+  );
+
+  const proceed = readline.question('Y/n\n');
+
+  if (!['y', 'yes'].includes(proceed.toLowerCase())) {
+    console.log('Bye')
+    return;
+  }
+
   const oPepe = await ethers.deployContract("OErc20Delegator", [
-    "0x6982508145454Ce325dDbE47a25d4ec3d2311933", // underlying token address (PEPE)
-    "0x7D61ed92a6778f5ABf5c94085739f1EDAbec2800", // Comptroller address
-    "0x4021047a36AC60b40316F630307cD4791cdAeD52", // Interest rate model address
-    0.2e9, // initial exchange rate mantissa
+    PEPE_ADDRESS, // underlying token address (PEPE)
+    COMPTROLLER_ADDRESS, // Comptroller address
+    TOKEN_JUMP_RATE_MODEL_ADDRESS, // Interest rate model address
+    DEFAULT_OTOKEN_INITIAL_EXCHANGE_RATE_MANTISSA, // initial exchange rate mantissa
     "Onyx PEPE",
     "oPEPE",
-    8, // oToken decimals
-    "0x08eDF0F2AF8672029eb445742B3b4072c6158DF3", // admin address (Timelock)
-    "0x9dcb6bc351ab416f35aeab1351776e2ad295abc4", // implementation address (OErc20Delegate)
+    DEFAULT_OTOKEN_DECIMALS, // oToken decimals
+    GOVERNANCE_TIMELOCK_ADDRESS, // admin address (Timelock)
+    OTOKEN_IMPLEMENTATION_ADDRESS, // implementation address (OErc20Delegate)
     "0x" // become implementation signature (empty)
   ]);
 
@@ -20,15 +43,15 @@ async function main() {
   const oPepeOracleFactory = await ethers.getContractFactory('UniV3TwapOracle') as UniV3TwapOracle__factory;
 
   const oPepeOracle = await oPepeOracleFactory.deploy(
-    "0x6982508145454ce325ddbe47a25d4ec3d2311933", // PEPE token
-    "0x11950d141ecb863f01007add7d1a342041227b58", // PEPE/ETH Uni V3 Pool
+    PEPE_ADDRESS, // PEPE token
+    PEPE_ETH_UNIV3_POOL, // PEPE/ETH Uni V3 Pool
     3600,
   );
 
   await oPepeOracle.waitForDeployment();
 
   await oPepeOracle.setAdmin(
-    "0x08eDF0F2AF8672029eb445742B3b4072c6158DF3" as any // Timelock address
+    GOVERNANCE_TIMELOCK_ADDRESS as any // Timelock address
   );
 
   console.log('Deployed oToken oPEPE, address: ', await oPepe.getAddress());
